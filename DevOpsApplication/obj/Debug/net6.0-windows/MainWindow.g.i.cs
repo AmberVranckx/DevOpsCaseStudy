eaaa9d84191a6a74bb09799cf1dd6fab27e85a31
AmberVranckx <r0938855@student.thomasmore.be>
2023-12-17 12:03:32 +0100

    update

--- DevOpsApplication/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ DevOpsApplication/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -309,7 +309,7 @@ namespace DevOpsApplication {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/DevOpsApplication;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/DevOpsApplication;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
